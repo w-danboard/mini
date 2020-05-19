9f51bfef87a673bf1f741b286fdf8d9aa5568dd2
wanglin <wanglin46@lenovo.com>
2020-05-19 18:15:57 +0800

    添加登录页

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,17 +3,50 @@ import Router from 'vue-router'
 
 Vue.use(Router);
 
+ // 登录
+ const login = () => 
+ import('@/views/login/index.vue')
+
+ // 页面不存在
+const exception_404 = () =>
+import('@/views/exception/exception_404.vue')
+
 const router = new Router({
   mode: 'history',
   routes: [
     {
-      path: '/index',
-      name: 'index',
-      component: () => import('@/views/index.vue'),
+      path: '/',
+      name: '/',
+      redirect: '/login'
+    },
+    {
+      path: '*',
+      component: exception_404
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login,
       meta: {
-        title: '首页'
+        name: '登录'
+      }
+    },
+    {
+      path: '/exception_404',
+      name: 'exception_404',
+      component: exception_404,
+      meta: {
+        name: '页面不存在'
       }
     }
+    // {
+    //   path: '/index',
+    //   name: 'index',
+    //   component: () => import('@/views/index.vue'),
+    //   meta: {
+    //     title: '首页'
+    //   }
+    // }
   ]
 })
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="app-login">
-    
+    <!-- 登录框 -->
+    <div class="logo">
+      <!-- model为表单数据对象 -->
+      <el-form ref="loginForm" :model="loginForm" label-width="0">
+        <el-form-item>
+          <el-input
+            v-model="loginForm.userName"
+            placeholder="请输入用户名"
+            autofocus
+            clearable>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.passWord"
+            type="password"
+            placeholder="请输入密码"
+            autofocus
+            clearable>
+          </el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" :loading="loading" @click="submit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'app-login',
+  data() {
+    return {
+      loading: false,
+      loginForm: {
+        userName: '',
+        passWord: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log('submit');
+    }
+  }
+}
+</script>
+<style lang="postcss" scoped>
+
+</style>
