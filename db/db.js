f46f4204bb14b337aaf8b0a603c118d62458ea86
wanglin26782 <wanlgin26782@163.com>
2020-05-20 01:04:50 +0800

    '定义用户模型'

--- db/db.js
+++ db/db.js
@@ -1,22 +1,22 @@
-// mongoose是一个ORM工具 就是对象模型 工具 可以让我们像操作对象一样操作数据库
-let mongoose = require('mongoose');
-let DB_URL = 'mongodb://127.0.0.1/school';  // 数据库地址
+// // mongoose是一个ORM工具 就是对象模型 工具 可以让我们像操作对象一样操作数据库
+// let mongoose = require('mongoose');
+// let DB_URL = 'mongodb://127.0.0.1/school';  // 数据库地址
 
-mongoose.connect(DB_URL);
+// mongoose.connect(DB_URL);
 
-// 连接成功
-mongoose.connection.on('connected', function() {
-  console.log('Mongoose connection open to' + DB_URL);
-});
+// // 连接成功
+// mongoose.connection.on('connected', function() {
+//   console.log('Mongoose connection open to' + DB_URL);
+// });
 
-// 连接异常
-mongoose.connection.on('error', function(err) {
-  console.log('Mongoose connection error' + err);
-});
+// // 连接异常
+// mongoose.connection.on('error', function(err) {
+//   console.log('Mongoose connection error' + err);
+// });
 
-// 连接断开
-mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose connection disconnected');
-});
+// // 连接断开
+// mongoose.connection.on('disconnected', function() {
+//   console.log('Mongoose connection disconnected');
+// });
 
-module.exports = mongoose;
+// module.exports = mongoose;
--- model/index.js
+++ model/index.js
@@ -1,2 +1,16 @@
 let mongoose = require('mongoose');
+// 连接数据库
 mongoose.connect('mongodb://127.0.0.1/school');
+
+// 定义用户集合的骨架模型 规定了用户集合中文档的属性和类型
+let userSchema = new mongoose.Schema({
+    userName: String,
+    passWord: String,
+    email: String
+});
+
+// 定义用户模型
+let user = mongoose.model('user', userSchema);
+
+// 把用户模型挂载到导出对象上
+module.exports.user = user;
